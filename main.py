3871453c19f9c5ae1a83bfa511b588f493f1a7f2
ssannssarr <ssantanusarkar8@gmail.com>
2026-07-19 22:33:08 +0530

    feat: add initial project scaffold with OpenAI API client

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from zuni!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/zuni/API/api.py
@@ -0,0 +1,71 @@
+"""Import Required dependenies"""
+from typing import Any
+from dataclasses import dataclass
+import httpx
+
+
+@dataclass(frozen=True, slots=True)
+class APIConfig:
+    API_KEY: str | None
+    BASE_URL: str | None
+    MODEL: str | None
+    TIMEOUT: int | None = 60
+
+
+class OpenAI:
+
+    def __init__(self, config: APIConfig):
+        self.api_key = config['API_KEY']
+        self.base_url = config['BASE_URL']
+        self.model = config['MODEL']
+        self.timeout = config['TIMEOUT']
+
+    def headers(self) -> dict[str, Any]:
+        if not self.api_key:
+            raise RuntimeError("API Ky not found in config")
+
+        return {
+            "Authoriztion": f"Bearer {self.api_key}",
+            "Content-Type": "application/json"
+        }
+
+    def payload(
+        self,
+        chat: list[dict[str, Any]] | None,
+        tools: list[dict[str, Any]] | None = None,
+    ) -> list[dict[str, Any]]:
+        payload = {
+            "model": self.model,
+            "messages": chat
+        }
+        if tools is not None:
+            payload["tools"] = tools
+
+        return payload
+
+    async def ask(
+        self,
+        chat: list[dict[str, Any]] | None,
+        tools: list[dict[str, Any]] | None = None,
+    ) -> list[dict[str, Any]]:
+
+        aclient = httpx.AsyncClient(
+            base_url=self.base_url,
+            timeout=self.timeout
+        )
+
+        response = await aclient.post(
+            "/v1/chat/completions",
+            headers=self.headers(),
+            json=self.payload(
+                chat=chat,
+                tools=tools
+            )
+        )
+
+        if response.status_code == 200:
+            return response.json(), True
+        return response.json(), False
+
+    async def aclose(self):
+        await self.aclient.aclose()
